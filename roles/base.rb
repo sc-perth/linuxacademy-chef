f6a440f7b1b8eb2b968b8438578d207583829a61
John Harlan <perth@semicosmic.net>
2016-04-23 07:10:01 +0000

    after chef-client supermarket

--- roles/base.rb
+++ roles/base.rb
@@ -1,3 +1,3 @@
 name "base"
 description "Role of recipies to be on all servers"
-run_list "recipe[security]", "recipe[localusers]"
+run_list "recipe[chef-client::delete_validation]", "recipe[chef-client::cron]", "recipe[chef-client]", "recipe[localusers]"
